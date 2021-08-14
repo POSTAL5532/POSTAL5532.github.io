07f6ff14627b374953e57706b7505bfc19da1ba9
Scherbachenya, Igor <evgeniionegin791@gmail.com>
2021-08-14 23:11:04 +0300

    Improved render resize.

--- js/solar-system/SceneEnvironment.js
+++ js/solar-system/SceneEnvironment.js
@@ -33,18 +33,17 @@ export class SceneEnvironment {
     initRenderer = () => {
         const canvas = document.querySelector("#canvas");
         this.renderer = new WebGLRenderer({canvas});
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
     initCamera = () => {
         this.camera = new PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 5000);
         this.camera.position.set(0, 2500, 0);
-        /*this.camera.up.set(0, 0, 1);
-        this.camera.lookAt(0, 0, 0);*/
     }
 
     initControls = () => {
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.enablePan = true;
+        this.controls.enableDamping = true;
         this.controls.update();
     }
 
@@ -56,8 +55,32 @@ export class SceneEnvironment {
         this.scene.add(object);
     }
 
+    resizeRendererToDisplaySize = () => {
+        const canvas = this.renderer.domElement;
+        /*const width = canvas.clientWidth;
+        const height = canvas.clientHeight;*/
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+
+        if (needResize) {
+            this.renderer.setSize(width, height, false);
+        }
+
+        return needResize;
+    }
+
+    updateCameraParams = () => {
+        if (this.resizeRendererToDisplaySize()) {
+            const canvas = this.renderer.domElement;
+            this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            this.camera.updateProjectionMatrix();
+        }
+    }
+
     animate = () => {
         !!this.animateCallback && this.animateCallback();
+        this.updateCameraParams();
         this.controls.update();
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(this.animate);
